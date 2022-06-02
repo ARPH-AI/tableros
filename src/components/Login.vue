57e0e70117d0c494fe0f8e8ea1b6d411db17d619
florblue <fotarola@forte.io>
2022-06-02 18:02:33 -0300

    Maquetando pagina de login

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-screen font-mono bg-cover bg-gradient-to-r from-primary via-third to-secondary">
+    <div class="container flex flex-1 justify-center items-center mx-auto h-full">
+      <div class="w-full max-w-lg">
+        <div class="leading-loose">
+          <form class="p-10 m-4 max-w-sm bg-white bg-opacity-25 rounded shadow-xl">
+            <p class="mb-4 text-2xl font-bold text-center text-white">Iniciar sesión</p>
+            <div class="">
+              <label class="block text-base text-left text-white" for="email">Correo electrónico</label>
+              <input
+                class="px-3 py-1 w-full text-left bg-white rounded  focus:secondary focus:ring-secondary focus:ring focus:ring-opacity-80 focus:outline-none"
+                type="email"
+                id="email"
+                placeholder="Correo electrónico"
+                aria-label="email"
+                required
+              />
+            </div>
+            <div class="mt-4">
+              <label class="block text-base text-left text-white">Contraseña</label>
+              <input
+                class="px-3 py-1 w-full text-left bg-white rounded  focus:secondary focus:ring-secondary focus:ring focus:ring-opacity-80 focus:outline-none"
+                type="password"
+                id="password"
+                placeholder="Contraseña"
+                arial-label="password"
+                required
+              />
+            </div>
+
+            <div class="flex mt-4">
+              <button
+                @click="$auth.login"
+                class="tracking-wider text-white rounded hoverfont-light hover:text-secondary"
+                type="submit"
+              >
+                Entrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-h-screen bg-light_base dark:bg-dark_base text-light_contrast dark:text-dark_contrast">
+  <div
+    v-if="$auth.isAuthenticated"
+    class="min-h-screen bg-light_base dark:bg-dark_base text-light_contrast dark:text-dark_contrast"
+  >
     <Dashboard />
   </div>
+  <span v-else><Login /></span>
 </template>
 
 <style></style>
