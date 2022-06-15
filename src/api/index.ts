dae9129c1e6309df2c1eae62547d50e438f147da
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-06-15 14:53:42 -0300

    Se agrega export de AuthApi en la api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
+import axiosInstance from './axios'
 import { OsmApi } from '@/api-client-osm'
 import { DatosgeoApi } from '@/api-client-datosgeo'
-import axiosInstance from './axios'
+import { AuthApi } from '@/api-client-auth'
 
-export const osmApi = new OsmApi(undefined, undefined, axiosInstance)
-export const datosgeoApi = new DatosgeoApi(undefined, undefined, axiosInstance)
+export const fetchClient = axiosInstance
+export const osmApi = new OsmApi(undefined, undefined, fetchClient)
+export const datosgeoApi = new DatosgeoApi(undefined, undefined, fetchClient)
+export const authApi = new AuthApi(undefined, undefined, fetchClient)
