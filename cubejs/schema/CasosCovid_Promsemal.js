81426829682cb98744effe0fdde5d359371bc21e
José Luis Di Biase <josx@interorganic.com.ar>
2022-08-04 17:00:07 -0300

    CasosCovid_Promsemal: Quitar buenos aires que estaba fijo en consulta
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- cubejs/schema/CasosCovid_Promsemal.js
+++ cubejs/schema/CasosCovid_Promsemal.js
@@ -2,7 +2,7 @@ cube(`casosCovidPromSem`, {
   sql: `select se.nombre_semana,se.numero_semana,se.fecha,(select count(1) from tableros.problema_con_covid as pcc1
   where not exists (select pcc2.id_consulta from tableros.problema_con_covid as pcc2 where pcc1.id_consulta!=pcc2.id_consulta and pcc1.patient_id=pcc2.patient_id and pcc1.start_date between pcc2.start_date and pcc2.start_date + interval '14 day' )
   and pcc1.description = 'Confirmado' and pcc1.start_date=se.fecha) as cantidad,
-  (select count(1) from tableros.snvs s where clasificacion_resumen = 'Confirmado' and se.fecha=s.fecha_apertura and residencia_provincia_nombre='Buenos Aires') as cantidad_snvs from tableros.semana_epidemiologica se
+  (select count(1) from tableros.snvs s where clasificacion_resumen = 'Confirmado' and se.fecha=s.fecha_apertura) as cantidad_snvs from tableros.semana_epidemiologica se
   `,
 
   measures: {
