053d80996db0a9adc89775b6db4ffd7a947c4315
José Luis Di Biase <josx@interorganic.com.ar>
2022-09-06 16:46:36 -0300

    Avoid console.log
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,6 @@ import { BackendApi } from '@/api-client-backend'
 
 // Backend base path
 const basePath = import.meta.env.VITE_BACKEND_API_URL
-console.log(basePath)
 export const fetchClient = axiosInstance
 export const osmApi = new OsmApi(undefined, undefined, fetchClient)
 export const datosgeoApi = new DatosgeoApi(undefined, undefined, fetchClient)
