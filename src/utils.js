360f5e055ae05e51ce4adfb1af95e89e4ba3b463
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-11-28 18:32:08 -0300

    Se agregan nuevas funciones de formateo de valores en utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+const LOCALE = 'es-AR'
 
 const marker = (args) => args.marker ? args.marker : ''
 
@@ -11,20 +12,38 @@ const bold = (txt) => `<b>${txt}</b>`
 
 const positive = (num) => num * -1
 
-const normalizeDecimals = (num) =>
-  new Intl.NumberFormat('es-AR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(num)
+const getName = (args) => args.name ? args.name : ''
 
+export const normalizeDecimals = (num, options={}) => {
+  return num ? new Intl.NumberFormat(LOCALE, {
+    ...options,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(num) : num
+}
+
+export const normalizeArgsDecimals = (args, options={}) => {
+  if (args) {
+    args.forEach(arg => {
+      arg.value = normalizeDecimals(arg.value, options)
+    })
+  }
+  return args
+}
 
 export const formatLeftDataToNaturalNumber = (args) => `
-  ${axisValueLabel(args[0])} <br/>
-  ${marker(args[0])} ${seriesName(args[0])}: ${bold(positive(value(args[0])))} <br/>
-  ${marker(args[1])} ${seriesName(args[1])}: ${bold(value(args[1]))}`
+  ${axisValueLabel(args[0])} <br/> 
+  ${marker(args[0])} ${seriesName(args[0])}: ${bold(positive(value(args[0])))} <br/> 
+  ${marker(args[1])} ${seriesName(args[1])}: ${bold(value(args[1]))}
+`
 
 export const formatDecimalValues = (args) => `
   ${axisValueLabel(args[0])} <br/>
-  ${marker(args[0])} ${seriesName(args[0])}: ${bold(normalizeDecimals(value(args[0])))} <br/>
-  ${marker(args[1])} ${seriesName(args[1])}: ${bold(normalizeDecimals(value(args[1])))}
+  ${marker(args[0])} ${seriesName(args[0])}: ${bold((normalizeDecimals(value(args[0])) || 0 )+' %')} <br/>
+  ${marker(args[1])} ${seriesName(args[1])}: ${bold((normalizeDecimals(value(args[1])) || 0 )+' %')}
+`
+
+export const formatDonaDecimalValues = (args) => `
+  ${getName(args)} <br/>
+  ${bold(normalizeDecimals(value(args))+' %')}
 `
