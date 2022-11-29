4d4d05665fd1111e2f7651ed643e87e885286e9d
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-11-29 11:03:22 -0300

    Fix estilos en GeoInfoCard

--- src/components/GeoInfoCard.vue
+++ src/components/GeoInfoCard.vue
@@ -19,15 +19,15 @@ const normalize = (property: any, type='') => {
     :class="`divide-y divide-dashed sm:p-2 xl:p-4 2xl:p-5 rounded-lg border-r-4 shadow-2xl bg-light_smooth dark:bg-dark_smooth dark:border-${props.colorTheme}_dark border-${props.colorTheme}`"
   >
     <div class="leading-tight text-left text-light_contrast dark:text-dark_contrast">
-      <h4 class="mt-2 text-base font-bold uppercase">
+      <h4 class="mt-2 text-base font-bold uppercase mb-3">
         {{ titulo }}
       </h4>
-      <h4 class="text-xs uppercase">TASA x 100 mil hab.</h4>
-      <h3 :class="`text-3xl font-semibold text-${props.colorTheme}`">
+      <h4 class="text-xs uppercase mb-1">TASA x 100 mil hab.</h4>
+      <h3 :class="`text-3xl font-semibold text-${props.colorTheme} mb-2`">
         {{ normalize(tasa, 'tasa') }}
       </h3>
       <h4 class="text-xs uppercase">CANT. CASOS ACTIVOS</h4>
-      <h3 :class="`text-3xl font-semibold text-${props.colorTheme}`">
+      <h3 :class="`text-3xl font-semibold text-${props.colorTheme} mb-2`">
         {{ normalize(cantidad) }}
       </h3>
     </div>
