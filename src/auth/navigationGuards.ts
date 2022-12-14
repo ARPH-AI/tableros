6faa717477afa8a3b24e7c21ab31229b3d6cce05
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-12-14 17:22:21 -0300

    refactor: Actualizacion de titulo con back button del navegador

--- src/auth/navigationGuards.ts
+++ src/auth/navigationGuards.ts
@@ -1,10 +1,22 @@
 import { RouteLocationRaw, Router } from 'vue-router'
 import { RequiredAuthOptions } from './types'
 import { useAuth } from './useAuth'
+import { useSectionsStore } from '@/stores/sections-store'
+import { storeToRefs } from 'pinia'
 
 export function configureNavigationGuards(router: Router, options: RequiredAuthOptions) {
   router.beforeEach(async (to): Promise<boolean | RouteLocationRaw> => {
     const auth = useAuth()
+    const sectionsStore = useSectionsStore()
+    const { currentSection, previousTitle } = storeToRefs(sectionsStore)
+
+    window.onpopstate = () => {
+      if (currentSection.value !== 'home' && previousTitle.value.length > 1) {
+        sectionsStore.popTitle()
+      } else {
+        sectionsStore.popTitle()
+      }
+    }
 
     if (to.name === options.loginRouteName) {
       if (auth.isAuthenticated) {
@@ -13,6 +25,7 @@ export function configureNavigationGuards(router: Router, options: RequiredAuthO
       return true
     }
 
+    sectionsStore.setAxisTitle(to.name)
     if (!to.meta.public) {
       if (!auth.isAuthenticated) {
         return { name: options.loginRouteName, query: { redirectTo: to.fullPath } }
