9a4f790971ff70ba06999c522017ae5d437b8e31
Leonardo Vaquel <leonardo.vaquel@camba.coop>
2022-12-14 17:19:16 -0300

    refactor: Se agregan informacion  a las stores de pinia

--- src/stores/enos-store.ts
+++ src/stores/enos-store.ts
@@ -1,19 +1,76 @@
+import { enos } from '@/data/enos/enos'
 import { defineStore } from 'pinia'
+import { data_sources } from '@/data/enos/enos'
+
+interface DATA_SOURCE {
+  label: string
+  key: string
+  title: string
+  content: string
+}
+
+interface ENO {
+  key: string
+  title: string
+  fuente_datos: DATA_SOURCE[]
+  main_page: string
+  componentes: {
+    situacion_actual: string
+    caracterizacion: string
+    geo: string
+  }
+  mainComponent: string
+}
+
+const default_eno: ENO = {
+  key: 'default',
+  title: 'Default',
+  fuente_datos: [],
+  main_page: 'seccion-situacion-actual',
+  componentes: {
+    situacion_actual: '',
+    caracterizacion: '',
+    geo: '',
+  },
+  mainComponent: ''
+}
 
 export const useEnosStore = defineStore('enosStore', {
   state: () => {
     return {
-      current_eno: 'covid',
+      current_eno: '',
+      enos_data: enos,
+      current_eno_data: default_eno,
     }
   },
   getters: {
     getCurrentEno(state) {
       return state.current_eno
     },
+    getCurrentEnoDataSources(state) {
+      if (state.current_eno !== '') {
+        return enos.find((eno) => eno.key === state.current_eno)?.fuente_datos
+      } else {
+        return data_sources
+      }
+    },
+    getCurrentEnoData(state) {
+      return state.current_eno_data
+    }
   },
   actions: {
-    setCurrentEno(state) {
+    setCurrentEno(state: { current_eno: any }) {
       state.current_eno = state
+    },
+    updateEnosDataBySource(source: string) {
+      const newData = enos.filter((eno) => eno.fuente_datos.some((s) => s.key === source))
+      this.enos_data = newData
+    },
+    setCurrentEnoString(value: string) {
+      this.current_eno = value
+    },
+    setCurrentEnoData(eno: ENO) {
+      this.current_eno_data = eno
     },
   },
 })
--- src/stores/sections-store.ts
+++ src/stores/sections-store.ts
@@ -1,16 +1,62 @@
 import { defineStore } from 'pinia'
-import { main_axis } from '@/constants'
+import { main_axis, sections } from '@/constants'
+
+const queue: string[] = []
+const section_ls = Object.entries(sections)
+const section_keys = section_ls.map((axis) => axis[1].key)
+const main_axis_keys = main_axis.map((axis): string => axis.key)
 
 export const useSectionsStore = defineStore('sectionsStore', {
-  state: () => ({ currentSection: 'home' }),
+  state: () => ({
+    currentSection: 'home',
+    sectionsData: main_axis,
+    axisTitle: '',
+    sectionTitle: '',
+    pageTitle: '',
+    previousTitle: queue,
+  }),
   getters: {
     getCurrentSection(state) {
       return state.currentSection
     },
+    getAxis(state) {
+      return state.sectionsData
+    },
+    getSectionTitle(state) {
+      return state.sectionTitle
+    },
+    getAxisTitle(state) {
+      return state.axisTitle
+    },
+    getPageTitle(state) {
+      return state.pageTitle
+    },
   },
   actions: {
-    setCurrentSection(state) {
-      state.currentSection = state
+    setCurrentSection(value: string) {
+      this.currentSection = value
+    },
+    setSectionTitle(value: string) {
+      this.previousTitle.push(value)
+      this.sectionTitle = value
+    },
+    setPreviuosTitle(queue: string[]) {
+      this.previousTitle = queue
+    },
+    popTitle() {
+      this.previousTitle.pop()
+      this.sectionTitle = this.previousTitle[this.previousTitle.length - 1]
+    },
+    setAxisTitle(value: string) {
+      if (section_keys.includes(value) || value === '') {
+        this.axisTitle = value
+      }
+      if (main_axis_keys.includes(value) || value === '') {
+        this.axisTitle = ''
+      }
+    },
+    setPageTitle(value: string) {
+      this.pageTitle = value
     },
   },
 })
