f1b549c9e3ccb2de3ad53bb300b3df0d06911242
José Luis Di Biase <josx@interorganic.com.ar>
2022-12-22 10:09:17 -0300

    fix(cubejs): Perfiles diferenciados por departamento (no para geo)
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- cubejs/cube.js
+++ cubejs/cube.js
@@ -1,68 +1,71 @@
 // Cube.js configuration options: https://cube.dev/docs/config
 module.exports = {
   queryRewrite: (query, { securityContext }) => {
-  // Tenemos en JWT `securityContext` `userId, userRole, userName, userDepartament`
-    // if (!securityContext.userId) {
-    //   throw new Error('No id found in Security Context!');
-    // }
+   // Tenemos en JWT `securityContext` `userId, userRole, userName, userDepartament`
+   if (!securityContext.userId) {
+      throw new Error('No id found in Security Context!');
+    }
 
-    // if (!securityContext.userRole) {
-    //   throw new Error('No role found in Security Context!');
-    // }
+    if (!securityContext.userRole) {
+      throw new Error('No role found in Security Context!');
+    }
 
-    // const validRoles = [ 'PERFIL_EDPIDEMIO_MESO', 'PERFIL_EPIDEMIO_INSTITUCION']
-    // if (!validRoles.includes(securityContext.userRole)) {
-    //   throw new Error('No valid role found in Security Context!');
-    // }
+    const validRoles = [ 'PERFIL_EPIDEMIO_MESO', 'PERFIL_EPIDEMIO_INSTITUCION']
+    if (!validRoles.includes(securityContext.userRole)) {
+      throw new Error('No valid role found in Security Context!');
+    }
 
-    // if (securityContext.userRole == 'PERFIL_EPIDEMIO_INSTITUCION') {
+    if (securityContext.userRole == 'PERFIL_EPIDEMIO_INSTITUCION') {
 
-    //   if (!securityContext.userDepartament) {
-    //     throw new Error('No encontro departamento en Security Context!');
-    //   }
+      if (!securityContext.userDepartment) {
+        throw new Error('No encontro departamento en Security Context!');
+      }
 
-    //   const cubeNames = [
-    //     ...(query.dimensions || []),
-    //     ...(query.measures || []),
-    //   ].map((e) => e.split('.')[0]);
+      const cubeNames = [
+        ...(query.dimensions || []),
+        ...(query.measures || []),
+      ].map((e) => e.split('.')[0]);
 
-    //   const cubesFilter = [
-    //     `casosComorbilidad`,
-    //     `casosEnfermedad`,
-    //     `casosCovidPromSem`,
-    //     `casos`,
-    //     `EnfermedadEdadSexoFem`,
-    //     `EnfermedadEdadSexo`,
-    //     `EnfermedadEdadSexoMasc`,
-    //     `CovidEdadSexoMascSNVS`,
-    //     `CovidEdadSexoFemSNVS`,
-    //     `CovidEdadSexoSNVS`,
-    //     `casosCovidPromSemSNVS`,
-    //     `casosFebriles`,
-    //     `casosDenguePromSem`,
-    //     `casosDenguePromSemSNVS`,
-    //     `DengueEdadSexoFemSNVS`,
-    //     `DengueEdadSexoMascSNVS`,
-    //     `DengueEdadSexoSNVS`
-    //   ]
+      const cubesFilter = [
+        `casosComorbilidad`,
+        `casosEnfermedad`,
+        `casosCovidPromSem`,
+        `casos`,
+        `EnfermedadEdadSexoFem`,
+        `EnfermedadEdadSexo`,
+        `EnfermedadEdadSexoMasc`,
+        `CovidEdadSexoMascSNVS`,
+        `CovidEdadSexoFemSNVS`,
+        `CovidEdadSexoSNVS`,
+        `casosCovidPromSemSNVS`,
+        `casosFebriles`,
+        `casosDenguePromSem`,
+        `casosDenguePromSemSNVS`,
+        `DengueEdadSexoFemSNVS`,
+        `DengueEdadSexoMascSNVS`,
+        `DengueEdadSexoSNVS`
+      ]
 
-    //   const cubesFilterMap = [
-    //     `CovidEdadSexoSNVS.Ciudad`,
-    //     `casos.ciudad`,
-    //   ]
-    //   const cubeDimensions = [
-    //     ...(query.measures || []),
-    //   ]
-    //   const filterKey = `departamento`
-    //   if (cubesFilter.includes(cubeNames[0]) {
-    //     if ( !cubeDimensions || !cubesFilterMap.includes(cubeDimensions[0])) {
-    //       query.filters.push({
-    //         member: `${cubeNames[0]}.${filterKey}`,
-    //         operator: 'equals',
-    //         values: [securityContext.userDepartament],
-    //       });
-    //     }
-    //   }
+      const cubesFilterMap = [
+        `CovidEdadSexoSNVS.Ciudad`,
+        `DengueEdadSexoSNVS.Ciudad`,
+        `casos.ciudad`,
+      ]
+      const cubeDimensions = [
+        ...(query.measures || []),
+      ]
+
+      const filterKey = `departamento`
+      if (cubesFilter.includes(cubeNames[0])) {
+        if ( !cubeDimensions || !cubesFilterMap.includes(cubeDimensions[0])) {
+          query.filters.push({
+            member: `${cubeNames[0]}.${filterKey}`,
+            operator: 'equals',
+            values: [securityContext.userDepartment],
+          });
+        }
+      }
+    }
 
     return query
   },
